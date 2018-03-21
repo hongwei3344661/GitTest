4fc7ebc46ded3924d64ad966bba2e53b46bea311
wangjiancheng <mr_jcwang@163.com>
2018-03-21 10:13:01 +0800

    20180321

--- src/TestClass.java
+++ src/TestClass.java
@@ -3,4 +3,7 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.println("this is a git test3");
     }
+    public void test1(){
+        System.out.println("hahaha");
+    }
 }
