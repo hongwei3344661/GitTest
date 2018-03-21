7e9304302829381a2632c6cc5682f762ddd14c45
wangjiancheng <mr_jcwang@163.com>
2018-03-21 10:58:36 +0800

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -4,7 +4,7 @@ public class TestClass {
         System.out.println("this is a git test3");
     }
     public void test1(){
-        System.out.println("ha1ha1haop ");
+        System.out.println("ha1ha1h1aop ");
     }
     public void test2(){
         System.out.println("hahaha2");
