dc71897fbd71430b850cbca0100b8240e3364330
wangjiancheng <mr_jcwang@163.com>
2018-03-31 16:48:25 +0800

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,16 +1,7 @@
 public class TestClass {
 
-    public static void main(String[] args) {
-        System.out.println("this is a git test3");
-    }
     public void test1(){
-        System.out.println("ha1ha1hao23p ");
-        System.out.println("ha1ha1h1aop ");
-    }
-    public void test2(){
-        System.out.println("hahaha2");
-    }
-    public void test3(){
-
+        System.out.println("这是主分支！！！ ");
+       
     }
 }
